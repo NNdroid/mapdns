b965590ceb8f11f9f9268c849ffbea1aac434aa0
NNdroid <99177648+NNdroid@users.noreply.github.com>
2024-05-22 22:31:53 +0800

    feat: try to support wild domain names

--- pkg/cache-srv/redis.go
+++ pkg/cache-srv/redis.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"mapdns/pkg/log"
+	"sort"
+	"strings"
 )
 
 const RedisPrefix = "mapdns"
@@ -24,6 +26,19 @@ func GetRedisKey(requestType uint16, domain string) string {
 	return fmt.Sprintf("%s:%d_%s", RedisPrefix, requestType, domain)
 }
 
+func GetAllUpLevelDomain(domain string, isContainSelf bool) []string {
+	var result []string
+	parts := strings.Split(domain, ".")
+	levelDomain := len(parts) - 1
+	for i := levelDomain; i >= 0; i-- {
+		if !isContainSelf && i == 0 {
+			break
+		}
+		result = append(result, strings.Join(parts[i:], "."))
+	}
+	return result
+}
+
 func (srv *Server) SetRecord(requestType uint16, domain, address string) error {
 	key := GetRedisKey(requestType, domain)
 	log.Logger().Debugf("setRecord -> Redis key: %s", key)
@@ -38,12 +53,45 @@ func (srv *Server) SetRecord(requestType uint16, domain, address string) error {
 func (srv *Server) GetRecord(requestType uint16, domain string) (string, bool) {
 	key := GetRedisKey(requestType, domain)
 	log.Logger().Debugf("getRecord -> Redis key: %s", key)
-	val, err := srv.rdb.Get(srv.ctx, key).Result()
+	//accuracy
+	var exists int64
+	var err error
+	exists, err = srv.rdb.Exists(srv.ctx, key).Result()
 	if err != nil {
 		return "", false
 	}
-	log.Logger().Debugf("getRecord -> domain (%s, %d): [%s]", domain, int(requestType), val)
-	return val, true
+	address := ""
+	log.Logger().Debugf("getRecord -> exists: [%d]", exists)
+	if exists > 0 {
+		address, err = srv.rdb.Get(srv.ctx, key).Result()
+		if err != nil {
+			return "", false
+		}
+	} else {
+		levelDomainArr := GetAllUpLevelDomain(domain, false)
+		sort.Slice(levelDomainArr, func(i, j int) bool {
+			return len(levelDomainArr[i]) < len(levelDomainArr[j])
+		})
+		log.Logger().Debugf("getRecord -> levelDomainArr: [%v]", levelDomainArr)
+		for _, levelDomain := range levelDomainArr {
+			rWildDomain := "*." + levelDomain
+			levelDomainCacheKey := GetRedisKey(requestType, rWildDomain)
+			log.Logger().Debugf("getRecord -> levelDomainCacheKey: [%s]", levelDomainCacheKey)
+			exists, err = srv.rdb.Exists(srv.ctx, levelDomainCacheKey).Result()
+			if err != nil {
+				continue
+			}
+			if exists > 0 {
+				address, err = srv.rdb.Get(srv.ctx, levelDomainCacheKey).Result()
+				if err != nil {
+					continue
+				}
+				log.Logger().Debugf("getRecord -> domain (%s, %d): [%s]", rWildDomain, int(requestType), address)
+			}
+		}
+	}
+	log.Logger().Debugf("getRecord -> domain (%s, %d): [%s]", domain, int(requestType), address)
+	return address, len(address) > 0
 }
 
 func (srv *Server) ClearRecords() error {
